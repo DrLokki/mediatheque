13d6b7dbf494a5d1c509b98a92e98f094fff00ab
DrLokki <lokki.steinsgate@ntymail.com>
2021-10-11 23:05:29 +0200

    Bugfix autoloader

--- src/Autoloader.php
+++ src/Autoloader.php
@@ -14,14 +14,16 @@ class Autoloader
 
 	static function register()
 	{
-		spl_autoload_register(__CLASS__, 'autoload');
+		spl_autoload_register(array(__CLASS__, 'autoload'));
 	}
 
 	static function autoload($class)
 	{
+
 		if (strpos($class, __NAMESPACE__.'\\') === 0 ) {
-			str_replace(__NAMESPACE__.'\\','',$class);
-			str_replace('\\','/',$class);
+			$class = str_replace(__NAMESPACE__.'\\','',$class);
+			$class = str_replace('\\','/',$class);
+			// var_dump($class);
 			require $class.'.php';
 		}
 	}
