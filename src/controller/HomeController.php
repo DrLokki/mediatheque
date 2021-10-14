fe3eb045da283c8685830a98f98b061f17009e8d
DrLokki <lokki.steinsgate@ntymail.com>
2021-10-14 02:45:22 +0200

    add deconexion function

--- src/controller/HomeController.php
+++ src/controller/HomeController.php
@@ -4,9 +4,15 @@ namespace Media\controller;
 use Media\model\Repository\RepositoryUsers;
 use Media\model\Entity\Users;
 
-?>
-
-<?php
+function check($buffer)
+{
+	if(isset($_SESSION['id'])){
+		
+		$buffer = str_replace('onclick="revelLogin()" type="button"', 'type="submit" value="deconect"', $buffer);
+		return (str_replace("connexion", "déconnexion", $buffer));
+	}
+	return $buffer;
+}
 
 class HomeController
 {
@@ -21,48 +27,14 @@ class HomeController
     {
     	ob_start();
     	include 'view/page/HomeView.php';
-    	$this->ob = ob_get_contents();
+    	$this->ob = check(ob_get_contents());
     	ob_end_clean();
-        $this->user = new Users();
-        $this->repository = new RepositoryUsers();
+
     }
 
 	public function index()
     {
     	echo $this->ob;
-
-    	try {
-			if ($_POST["button"] === "register") {
-				$this->user->setName($_POST['firstname'])
-					->setLastName($_POST['lastname'])
-					->setRole('inscrit')
-					->setEmail($_POST['email'])
-					->setBdate($_POST['birthDate'])
-					->setAdress($_POST['adress'])
-					->setPassword($_POST['password'])
-					->setIsVerified(false);
-				$this->repository->newUsers($this->user);
-				unset($_POST);
-			}elseif ($_POST["button"] === "login") {
-				$this->user = $this->repository->findOneByEmailPass($_POST['email'],$_POST['password']);
-				unset($_POST);
-				if ($this->user) {
-					if ($this->user->getIsVerified()) {
-						$_SESSION['firstname'] = $this->user->getName();
-						$_SESSION['lastname'] = $this->user->getLastName();
-						$_SESSION['id'] = $this->user->getId();
-						$_SESSION['loan_number'] = $this->user->getLoanNumber();
-						$_SESSION['role'] = $this->user->getRole();
-					}
-				}else
-				{
-						$_SESSION['role'] = "qsdfqsd";
-				}
-				
-			}
-		} catch (Exception $e) {
-			return $e;
-		}
 	}
 }
 ?>
--- src/view/component/header.php
+++ src/view/component/header.php
@@ -1,6 +1,8 @@
 <img src="https://source.unsplash.com/1440x400/?library" alt="" class="">
 <nav class="flex justify-between my-10">
-	<button onclick="revelLogin()" class="hover:bg-gray-300 hover:text-black flex items-center justify-center mx-8 sm:mx-10 my-2 px-4 h-8 text-sm text-white bg-black">connexion </button>
+	<form action="" method="post">
+		<button onclick="revelLogin()" type="button" class="hover:bg-gray-300 hover:text-black flex items-center justify-center mx-8 sm:mx-10 my-2 px-4 h-8 text-sm text-white bg-black" name="button"> connexion </button>
+	</form>
 	<div class="flex items-center">
 		<a href="" class="flex px-2 border-2 border-white hover:border-gray-300">mon compte</a>
 		<a href="" class="flex px-2 border-2 border-white hover:border-gray-300">catalogue</a>
