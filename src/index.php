2c323adc328d54c95c1b85f1b49f6685d00ce014
DrLokki <lokki.steinsgate@ntymail.com>
2021-10-14 02:46:29 +0200

    add post request manager

--- src/index.php
+++ src/index.php
@@ -1,4 +1,60 @@
 <?php
+session_start();
+use Media\Autoloader;
 
 require 'Autoloader.php';
 Autoloader::register();
+
+use Media\controller\HomeController;
+use Media\controller\PanelController;
+use Media\model\Entity\Users;
+use Media\model\Repository\RepositoryUsers;
+
+$home = new HomeController();
+$repository = new RepositoryUsers();
+$user = new Users();
+$panel = new PanelController();
+var_dump($_SESSION);
+// echo "<pre>";
+// var_dump($repository->findUnVerified());
+// echo '</pre>';
+// $home->index();
+$panel->index();
+
+try {
+	if (isset($_POST["button"])) {
+		if ($_POST["button"] === "register") {
+			$user->setName($_POST['firstname'])
+				->setLastName($_POST['lastname'])
+				->setRole('inscrit')
+				->setEmail($_POST['email'])
+				->setBdate($_POST['birthDate'])
+				->setAdress($_POST['adress'])
+				->setPassword($_POST['password'])
+				->setIsVerified(false);
+			$repository->newUsers($user);
+			unset($_POST);
+		}elseif ($_POST["button"] === "login") {
+			$user = $repository->findOneByEmailPass($_POST['email'],$_POST['password']);
+			unset($_POST);
+			if ($user) {
+				if ($user->getIsVerified()) {
+					$_SESSION['firstname'] = $user->getName();
+					$_SESSION['lastname'] = $user->getLastName();
+					$_SESSION['id'] = $user->getId();
+					$_SESSION['loan_number'] = $user->getLoanNumber();
+					$_SESSION['role'] = $user->getRole();
+				}
+			}else
+			{
+					$_SESSION['role'] = "qsdfqsd";
+			}
+			
+		}elseif ($_POST["button"] === "deconect") {
+			$_SESSION = [];
+			unset($_SESSION);
+		}
+	}
+} catch (Exception $e) {
+	return $e;
+}
